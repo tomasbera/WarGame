75efe24547c53d9f25ab511d7fc082ebf0e48ca9
Tomas Beranek <tomaber@stud.ntnu.no>
2022-03-30 19:43:06 +0200

    Test: added new tests for armyFiles

--- src/test/java/BattleSimulation/armyFilesTest.java
+++ src/test/java/BattleSimulation/armyFilesTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.NoSuchFileException;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class armyFilesTest {
@@ -36,7 +37,7 @@ public class armyFilesTest {
         }
 
         @Test
-        public void Test_if_makeCVSFile_throws_IllegalArgumentException(){
+        public void Test_if_makeCVSFile_throws_IllegalArgumentException_when_a_file_already_exists(){
             Army armyFileExisting = new Army("ExsistingFiletest");
             assertThrows(IllegalArgumentException.class, () -> {
                 armyFiles.makeCSVFile(armyFileExisting);
@@ -61,15 +62,40 @@ public class armyFilesTest {
         }
 
         @Test
-        public void Test_if_readFromCSV_throws_NullPointerException(){
+        public void Test_if_readFromCSV_throws_NullPointerException_when_file_has_no_units(){
             assertThrows(NullPointerException.class, () -> {
                 armyFiles.readFromCSV("NullArmyTest");
             });
         }
 
-        //TODO: check if the file exists, check if file is of csv format and if it has all needed attributes
+        @Test
+        public void Test_if_readFromCSV_throws_IllegalArgumentException_when_file_not_readable(){
+            assertThrows(IllegalArgumentException.class, () -> {
+                armyFiles.readFromCSV("NotReadableFile");
+            });
+        }
 
+        @Test
+        public void Test_if_readFromCSV_throws_NoSuchFileException_when_a_file_doenst_exits(){
+            assertThrows(NoSuchFileException.class, () -> {
+                armyFiles.readFromCSV("NoFileExist");
+            });
+        }
 
+        @Test
+        public void Test_if_file_has_units_and_throws_if_not(){
+            assertThrows(NullPointerException.class, () -> {
+                armyFiles.readFromCSV("NullArmyTest");
+            });
+        }
+
+        /*
+        @Test
+        public void Test_if_file_is_corrupt_or_has_illegal_characters_throw_IllegalArgumentException(){
+            assertThrows(IllegalArgumentException.class, () -> {
+                armyFiles.readFromCSV("IllegalCharacters");
+            });
+        }
+         */
     }
-
 }
